9a958600f6deba24f0f03524b4195ef342ea5dc8
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2025-07-11 08:04:53 +0200

    Props traning with default values and destructuring, ternary operator

--- 02-props/src/main.jsx
+++ 02-props/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDom from 'react-dom/client'
+import { LinkComponent } from './components/LinkComponent.jsx'
+import { DescriptionComponent } from './components/DescriptionComponent.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const element = (
+  <div>
+    <h1>Nasza strona</h1>
+    <main>
+      <LinkComponent text="http://zrozumreact.pl" shouldOpenNewTab={true}>
+        Strona testowa
+      </LinkComponent>
+      <DescriptionComponent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex ad
+        minima vitae perferendis laboriosam animi aliquid eius alias iste rerum,
+        aut, cum fugit! Odit obcaecati accusantium maxime iure quo.
+      </DescriptionComponent>
+      <LinkComponent text="http://zrozumreact.pl" shouldOpenNewTab={false}>
+        Strona testowa
+      </LinkComponent>
+      <DescriptionComponent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex ad
+        minima vitae perferendis laboriosam animi aliquid eius alias iste rerum,
+        aut, cum fugit! Odit obcaecati accusantium maxime iure quo.
+      </DescriptionComponent>
+      <LinkComponent text="http://zrozumreact.pl" shouldOpenNewTab={true}>
+        Strona testowa
+      </LinkComponent>
+      <DescriptionComponent>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex ad
+        minima vitae perferendis laboriosam animi aliquid eius alias iste rerum,
+        aut, cum fugit! Odit obcaecati accusantium maxime iure quo.
+      </DescriptionComponent>
+    </main>
+  </div>
 )
+
+ReactDom.createRoot(document.getElementById('root')).render(element)
