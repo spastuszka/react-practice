97e1317faafead02d28676de0dbec38d3d4bbeef
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2025-07-25 07:03:27 +0200

    Initial project - coniditional rendering

---
+++ 04-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div>
+      <h1>Conditional Rendering Example</h1>
+      <p>This is a simple example of conditional rendering in React.</p>
+    </div>
+  )
+}
---
+++ 04-conditional-rendering/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDom from 'react-dom/client'
+import React from 'react'
+import { App } from './components/App'
+
+ReactDom.createRoot(document.getElementById('root')).render(<App />)
