b2106cc956d5cc033b5920834905fbfef3a7f95a
Sebastian Pastuszka <s.pastuszka08@gmail.com>
2025-07-28 07:40:54 +0200

    Conditional rednering and resuasble component

--- 04-conditional-rendering/src/components/App.jsx
+++ 04-conditional-rendering/src/components/App.jsx
@@ -1,8 +1,31 @@
+import { PersonInfo } from './PersonInfo'
+
 export function App() {
+  const John = {
+    name: 'John',
+    age: 30,
+    mail: 'john.doe@test.com',
+    tel: '123-456-7890',
+  }
+  const Kate = {
+    name: 'Kate',
+    age: 25,
+    mail: 'kate.test@test.com',
+    tel: '032-456-7890',
+  }
+  const Karol = {
+    name: 'Karol',
+    age: 27,
+    mail: 'karol.test@test.com',
+  }
   return (
-    <div>
-      <h1>Conditional Rendering Example</h1>
-      <p>This is a simple example of conditional rendering in React.</p>
-    </div>
+    <>
+      <h2>First Contact</h2>
+      <PersonInfo person={John} />
+      <h2>Second Contact</h2>
+      <PersonInfo person={Kate} />
+      <h2>Third Contact</h2>
+      <PersonInfo person={Karol} />
+    </>
   )
 }
